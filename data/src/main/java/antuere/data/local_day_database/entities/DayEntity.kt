46a2bcc966b0dcc97b44fc43aa1d30d4c43c6e42
makeevrserg <makeevrserg@gmail.com>
2023-01-25 20:33:10 +0300

    removed redundant spaces in DayEntity; replaced var to val in SettingsEntity

--- data/src/main/java/antuere/data/local_day_database/entities/DayEntity.kt
+++ data/src/main/java/antuere/data/local_day_database/entities/DayEntity.kt
@@ -8,22 +8,16 @@ import antuere.domain.util.TimeUtility
 
 @Entity(tableName = DayEntity.TABLE_NAME)
 data class DayEntity(
-
     @PrimaryKey(autoGenerate = false)
     var dayId: Long = TimeUtility.parseCurrentTime().time,
-
     @ColumnInfo(name = "smile_image")
     val smileImage: SmileImage,
-
     @ColumnInfo(name = "day_text")
     var dayText: String,
-
     @ColumnInfo(name = "date_text")
     val dateString: String = TimeUtility.formatCurrentTime(),
-
     @ColumnInfo(name = "is_favorite")
     var isFavorite: Boolean = false
-
 ) {
     companion object {
         const val TABLE_NAME = "day_table"
--- data/src/main/java/antuere/data/preferences_data_store/settings_data_store/entities/SettingsEntity.kt
+++ data/src/main/java/antuere/data/preferences_data_store/settings_data_store/entities/SettingsEntity.kt
@@ -1,8 +1,7 @@
 package antuere.data.preferences_data_store.settings_data_store.entities
 
-data class SettingsEntity
-    (
-    var isBiometricEnabled: Boolean,
-    var isPinCodeEnabled: Boolean,
-    var isShowWorriedDialog: Boolean
+data class SettingsEntity(
+    val isBiometricEnabled: Boolean,
+    val isPinCodeEnabled: Boolean,
+    val isShowWorriedDialog: Boolean
 )
